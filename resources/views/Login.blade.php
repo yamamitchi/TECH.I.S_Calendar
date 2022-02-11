7f99fc6afd22aa4686eca83130a2083628cdb289
yamamitchi <yamamitchi@gmail.com>
2022-02-12 01:43:08 +0900

    make it better

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -46,6 +46,7 @@
         </div>
         <br>
         <div class="item"style="width:53%">
+            @csrf
             <button type="submit" style="font-size: 0.8rem;" class="btn btn--yellow btn--cubic">ログイン</button>
         </div>
 
