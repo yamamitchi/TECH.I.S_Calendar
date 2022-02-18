9c9aed21e4fbaf8e30f8b847a11d554b87254dc5
yamamitchi <yamamitchi@gmail.com>
2022-02-19 01:25:06 +0900

    message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,19 +1,19 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
-<script src=require_once __DIR__ . ('/js/fullcalendar/core/main.js')></script>
-<script src=require_once __DIR__ . ('/js/fullcalendar/daygrid/main.js')></script>
-<script src=require_once __DIR__ . ('/js/fullcalendar/interaction/main.js')></script>
-<script src=require_once __DIR__ . ('/js/fullcalendar/timegrid/main.js')></script>
-<script src=require_once __DIR__ . ('/js/fullcalendar/list/main.js')></script>
+<script src='/js/fullcalendar/core/main.js'></script>
+<script src='/js/fullcalendar/daygrid/main.js'></script>
+<script src='/js/fullcalendar/interaction/main.js'></script>
+<script src='/js/fullcalendar/timegrid/main.js'></script>
+<script src='/js/fullcalendar/list/main.js'></script>
 
-<script src=require_once __DIR__ . ("/js/ajax-setup.js")></script>
+<script src="/js/ajax-setup.js"></script>
 <!-- <script src='/js/fullcalendar.js'></script> -->
-<script src=require_once __DIR__ . ('/js/event-control.js')></script>
+<script src='/js/event-control.js'></script>
 
-<link href=require_once __DIR__ . ('/css/fullcalendar/core/main.css') type="text/css" rel='stylesheet' />
-<link href=require_once __DIR__ . ('/css/fullcalendar/daygrid/main.css') type="text/css" rel='stylesheet' />
-<link href=require_once __DIR__ . ('/css/fullcalendar/timegrid/main.css') type="text/css" rel='stylesheet' />
-<link href=require_once __DIR__ . ('/css/fullcalendar/list/main.css') type="text/css" rel='stylesheet' />
+<link href='/css/fullcalendar/core/main.css' type="text/css" rel='stylesheet' />
+<link href='/css/fullcalendar/daygrid/main.css' type="text/css" rel='stylesheet' />
+<link href='/css/fullcalendar/timegrid/main.css' type="text/css" rel='stylesheet' />
+<link href='/css/fullcalendar/list/main.css' type="text/css" rel='stylesheet' />
 <style>
         @media screen and (max-width: 767px){
             .fc-toolbar h2 {
