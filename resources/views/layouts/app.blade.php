ef05a791743f6c7e4538852d8764111ec136a648
yamamitchi <yamamitchi@gmail.com>
2022-02-19 00:30:01 +0900

    message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,19 +1,19 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
-<script src="{{ asset'/js/fullcalendar/core/main.js') }}"></script>
-<script src="{{ asset'/js/fullcalendar/daygrid/main.js') }}"></script>
-<script src="{{ asset'/js/fullcalendar/interaction/main.js') }}"></script>
-<script src="{{ asset'/js/fullcalendar/timegrid/main.js') }}"></script>
-<script src="{{ asset'/js/fullcalendar/list/main.js') }}"></script>
+<script src="{{ asset('/js/fullcalendar/core/main.js')}}"></script>
+<script src="{{ asset('/js/fullcalendar/daygrid/main.js')}}"></script>
+<script src="{{ asset('/js/fullcalendar/interaction/main.js')}}"></script>
+<script src="{{ asset('/js/fullcalendar/timegrid/main.js')}}"></script>
+<script src="{{ asset('/js/fullcalendar/list/main.js')}}"></script>
 
-<script src="{{ asset'/js/ajax-setup.js') }}"></script>
+<script src="{{ asset('/js/ajax-setup.js')}}"></script>
 <!-- <script src='/js/fullcalendar.js'></script> -->
-<script src="{{ asset'/js/event-control.js') }}"></script>
+<script src="{{ asset('/js/event-control.js')}}"></script>
 
-<link href="{{ asset'/css/fullcalendar/core/main.css') }}" type="text/css" rel='stylesheet' />
-<link href="{{ asset'/css/fullcalendar/daygrid/main.css') }}" type="text/css" rel='stylesheet' />
-<link href="{{ asset'/css/fullcalendar/timegrid/main.css') }}" type="text/css" rel='stylesheet' />
-<link href="{{ asset'/css/fullcalendar/list/main.css') }}" type="text/css" rel='stylesheet' />
+<link href="{{ asset('/css/fullcalendar/core/main.css')}}" type="text/css" rel='stylesheet' />
+<link href="{{ asset('/css/fullcalendar/daygrid/main.css')}}" type="text/css" rel='stylesheet' />
+<link href="{{ asset('/css/fullcalendar/timegrid/main.css')}}" type="text/css" rel='stylesheet' />
+<link href="{{ asset('/css/fullcalendar/list/main.css')}}" type="text/css" rel='stylesheet' />
 <style>
         @media screen and (max-width: 767px){
             .fc-toolbar h2 {
