ecd08efa2ad3f5819c23cca6af30ee247877d448
yamamitchi <yamamitchi@gmail.com>
2022-03-01 19:19:00 +0900

    make it better

--- resources/views/event_input.blade.php
+++ resources/views/event_input.blade.php
@@ -51,7 +51,7 @@
                     </div>
                     <div class="time_input">
                         <h3>終了時間</h3>
-                        <input type="text" min="09：00" max="17：30" step="900" name="end_time" class="time end" value="{{ old('end_time') }}">
+                        <input type="text"  step="900" name="end_time" class="time end" value="{{ old('end_time') }}">
                     </div>
                 </div>
                 <div class="item-input">
